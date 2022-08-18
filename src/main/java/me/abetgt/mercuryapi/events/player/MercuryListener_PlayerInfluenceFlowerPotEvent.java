89b6ae34ae63c522cd50f887dbb0b19ef8e8d8c5
AbeTGT <69663458+AbeTGT@users.noreply.github.com>
2022-08-19 08:36:38 +1200

    Fixed casting issues

--- src/main/java/me/abetgt/mercuryapi/events/player/MercuryListener_PlayerInfluenceFlowerPotEvent.java
+++ src/main/java/me/abetgt/mercuryapi/events/player/MercuryListener_PlayerInfluenceFlowerPotEvent.java
@@ -10,31 +10,29 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
-
+import org.bukkit.plugin.Plugin;
 
 
 public final class MercuryListener_PlayerInfluenceFlowerPotEvent implements Listener {
-    MercuryAPI mercuryPlugin = MercuryAPI.getMercuryPlugin();
-
+    Plugin mercuryPlugin = MercuryAPI.getMercuryPlugin();
     @EventHandler
     public void onInteractBlock(PlayerInteractEvent event){
         Block eventBlock = event.getClickedBlock();
         if (event.getAction() == Action.RIGHT_CLICK_BLOCK && eventBlock.getBlockData().getMaterial() == Material.FLOWER_POT){
             EquipmentSlot eventHand = event.getHand();
             ItemStack heldItem = event.getItem();
-            ItemStack clickedItemBlock = (ItemStack) eventBlock;
             if (!event.hasItem()){return;}
             Bukkit.getScheduler().scheduleSyncDelayedTask(mercuryPlugin, new Runnable() {
                 @Override
                 public void run() {
-                    String setType = clickedItemBlock.getType().name();
+                    String setType = eventBlock.getType().name();
                     if (setType.startsWith("POTTED") || setType.startsWith("potted")) {
                         PlayerInfluenceFlowerPotEvent flowerPotEvent = new PlayerInfluenceFlowerPotEvent(
                                 event.getPlayer(),
                                 eventBlock,
                                 eventHand,
                                 heldItem,
-                                clickedItemBlock.getType()
+                                eventBlock.getType()
                         );
                     }
                 }
